c43cdc5c04ad837235816309fdc636c7cf6e4f81
shashwat mishra <shashwatmishraog@gmail.com>
2022-08-18 19:26:06 +0530

    daily

--- dsa_love_babbar/dublicate_string.c
+++ dsa_love_babbar/dublicate_string.c
@@ -2,11 +2,19 @@
 #include<string.h>
 int main()
 {
-    char s[1000],c[1000];
+    char s[1000];
+    int t=0;
     gets(s);
     for(int i=0;i<strlen(s)-1;i++)
     {
-         
+        for(int j=0;j<strlen(s)-1;j++)
+        {
+            if(s[i]==s[j])
+            {
+                t++;
+            }
+        }
     }
+    printf("%d",t-strlen(s-1));
     return 0;
 }

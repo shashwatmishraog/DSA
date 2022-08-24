2a73bfba03901d04c61fa0b54d8ec1c2df326648
shashwat mishra <shashwatmishraog@gmail.com>
2022-08-24 20:11:21 +0530

    daily

--- dsa_love_babbar/roman_to_decimal.c
+++ dsa_love_babbar/roman_to_decimal.c
@@ -1,46 +1,100 @@
+
 #include<stdio.h>
 #include<string.h>
+int value(char r)
+{
+	if (r == 'I')
+		return 1;
+	if (r == 'V')
+		return 5;
+	if (r == 'X')
+		return 10;
+	if (r == 'L')
+		return 50;
+	if (r == 'C')
+		return 100;
+	if (r == 'D')
+		return 500;
+	if (r == 'M')
+		return 1000;
+	return 0;
+}
 int main()
 {
-    char str[100];
+	char str[10];
+    int s1,s2 ;
     gets(str);
-    int n=strlen(str);
-    int arr[n];
-    int i=0,sum=0;
-    label: switch(str[i])
-    {
-        case 'I':
-                 arr[i]=1;
-                 break;
-        case 'V':
-                 arr[i]=5;
-                 break;
-         case 'X':
-                 arr[i]=10;
-                 break;
+    int res = 0;
+	for (int i = 0; i < strlen(str); i++)
+	{
+		 s1 = value(str[i]);
 
-        case 'L':
-                 arr[i]= 50;
-                 break;
-        case 'C':
-                 arr[i]=100;
-                 break;
-        case 'D':
-                 arr[i]=500;
-                 break;
-        case 'M':
-                 arr[i]=1000;
-                 break;
-                 
-    }
-    if(i=!n)
-    {
-        i++;
-        goto label;
-    }
-    for(int i=0;i<n;i++)
-    {
-        sum=sum+arr[i];
-    }
-    printf("\n%d\n",sum);
+		if (i + 1 < strlen(str))
+		{
+			 s2 = value(str[i + 1]);
+			if (s1 >= s2)
+			{
+				res = res + s1;
+			}
+			else
+			{
+
+				res = res + s2 - s1;
+				i++;
+			}
+		}
+		else {
+			res = res + s1;
+		}
+	}
+    printf("%d",res);
+	return 0;
 }
+
+// #include<stdio.h>
+// #include<string.h>
+// int main()
+// {
+//     char str[100];
+//     gets(str);
+//     int n=strlen(str);
+//     int arr[n];
+//     int i=0,sum=0;
+//     label: switch(str[i])
+//     {
+//         case 'I':
+//                  arr[i]=1;
+//                  break;
+//         case 'V':
+//                  arr[i]=5;
+//                  break;
+//          case 'X':
+//                  arr[i]=10;
+//                  break;
+
+//         case 'L':
+//                  arr[i]= 50;
+//                  break;
+//         case 'C':
+//                  arr[i]=100;
+//                  break;
+//         case 'D':
+//                  arr[i]=500;
+//                  break;
+//         case 'M':
+//                  arr[i]=1000;
+//                  break;
+                 
+//     }
+//     if(i=!n)
+//     {
+//         i++;
+//         goto label;
+//     }
+//     for(int i=0;i<n;i++)
+//     {
+//         printf("%d",arr[i]);
+//         sum=sum+arr[i];
+//     }
+//     printf("\n%d\n",sum);
+// }

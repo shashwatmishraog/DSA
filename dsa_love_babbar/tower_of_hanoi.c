ab5779a16ee268e983ce900fb74a78d722978d85
shashwat mishra <shashwatmishraog@gmail.com>
2022-09-11 03:13:40 +0530

    daily

--- dsa_love_babbar/tower_of_hanoi.c
+++ dsa_love_babbar/tower_of_hanoi.c
@@ -2,7 +2,6 @@
 int c=0;
 int tower(int n,char source,char helper,char destination)
 {
-    c++;
     if(n==0)
     {
         return 0;
@@ -10,7 +9,8 @@ int tower(int n,char source,char helper,char destination)
     tower(n-1,source,destination,helper);
     printf("Moved element %d from tower %c to tower %c\n",n,source,destination);
     tower(n-1,helper,destination,source);
-    return c;
+    c++;
+    return 1;
 }
 int main()
 {
@@ -18,6 +18,8 @@ int main()
     printf("Enter the no. of elements\n");
     scanf("%d",&n);
     tower(n,'a','b','c');
-    printf("the no. of steps required was %d",tower(n,'a','b','c'));
+    printf("the no. of steps required was %d",c);
     return 0;
 }
+
+// the optimal soln. of the problem is of (2^n)-1 steps so we have to take that in consideration.
